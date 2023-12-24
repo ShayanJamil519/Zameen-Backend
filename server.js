b3aff9e858d173a1c93ba9546c257ac68aa3dafd
ShayanJamil519 <shayanjamil500@gmail.com>
2023-12-24 11:38:45 +0500

    CORS ISsues

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const connectDataBase = require("./config/database");
 const dotenv = require("dotenv");
+const cors = require("cors");
 const cloudinary = require("cloudinary");
 const errorMiddleware = require("./middleware/error");
 const express = require("express");
@@ -8,6 +9,7 @@ const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const fileUpload = require("express-fileupload");
 
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
